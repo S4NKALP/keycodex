9da1f36e172b292078cf4a7833c158a8631aed10
S4NKALP <sankalptharu50028@gmail.com>
2025-07-25 15:09:36 +0545

    ✨ feat: add configurations for typescript and python language servers

--- lsp/typescript.lua
+++ lsp/typescript.lua
@@ -1,5 +1,5 @@
 return {
-    cmd = { 'tailwindcss-language-server' },
+    cmd = { 'typescript-language-server', '--stdio' },
     root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' },
     filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
     init_options = {
