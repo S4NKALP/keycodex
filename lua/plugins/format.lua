9207dfa01f716542e53c83a08c355d6a8208c3b6
S4NKALP <sankalptharu50028@gmail.com>
2025-09-19 21:06:31 +0545

    feat: add treesj plugin for structural editing and biome for typescriptreact formatting

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -30,6 +30,7 @@ return {
                 ['python'] = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
                 ['javascript'] = { 'biome' },
                 ['typescript'] = { 'biome' },
+                ['typescriptreact'] = { 'biome' },
                 ['markdown'] = { 'prettier', 'markdownlint-cli2', 'markdown-toc' },
                 ['markdown.mdx'] = { 'prettier', 'markdownlint-cli2', 'markdown-toc' },
                 ['mdx'] = { 'prettier' },
