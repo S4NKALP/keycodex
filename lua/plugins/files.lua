295a1d9e093dc5fe49d96693ea4397797cfe6e7f
S4NKALP <sankalptharu50028@gmail.com>
2025-09-29 13:12:53 +0545

    feat(files): replace netrw with nvim-tree for improved file exploration

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -40,7 +40,24 @@ return {
         end,
     },
     {
-        'prichrd/netrw.nvim',
-        opts = {},
+        'nvim-tree/nvim-tree.lua',
+        version = '*',
+        lazy = false,
+        config = function()
+            require('nvim-tree').setup({
+                sort = {
+                    sorter = 'case_sensitive',
+                },
+                view = {
+                    width = 30,
+                },
+                renderer = {
+                    group_empty = true,
+                },
+                filters = {
+                    dotfiles = true,
+                },
+            })
+        end,
     },
 }
