f310464253b49ec02f132058a2b8a54eea92a0c4
S4NKALP <sankalptharu50028@gmail.com>
2025-10-16 23:03:56 +0545

    feat(git): add messenger.nvim for floating commit window

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,22 @@
 return {
-
+    -- FLOATING COMMIT WINDOW
+    {
+        'lsig/messenger.nvim',
+        enabled = false,
+        opts = {
+            border = 'rounded',
+        },
+        keys = {
+            {
+                '<leader>gc',
+                '<Cmd>MessengerShow<CR>',
+                desc = 'show commit in floating window',
+                mode = 'n',
+                noremap = true,
+                silent = true,
+            },
+        },
+    },
     -- git signs
     {
         'lewis6991/gitsigns.nvim',
@@ -23,8 +40,8 @@ return {
                 },
                 signs_staged_enable = true,
                 signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-                numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-                linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+                numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+                linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
                 word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
                 watch_gitdir = {
                     follow_files = true,
@@ -43,7 +60,7 @@ return {
                 current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
                 sign_priority = 6,
                 update_debounce = 100,
-                status_formatter = nil, -- Use default
+                status_formatter = nil,  -- Use default
                 max_file_length = 40000, -- Disable if file is longer than this (in lines)
                 preview_config = {
                     -- Options passed to nvim_open_win
