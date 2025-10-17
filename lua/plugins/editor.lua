b56ebf522878c6411ad7ec17f620d34caadf0375
S4NKALP <sankalptharu50028@gmail.com>
2025-10-17 15:39:32 +0545

    feat(editor): add editor plugins and configurations

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,75 +1,76 @@
 return {
     -- gitignore generator
     {
-        "wintermute-cell/gitignore.nvim",
-        cmd = { "Gitignore" },
+        'wintermute-cell/gitignore.nvim',
+        cmd = { 'Gitignore' },
         opts = {},
     },
 
     -- git signs
     {
-        "lewis6991/gitsigns.nvim",
-        events = { "BufReadPost", "BufNewFile" },
+        'lewis6991/gitsigns.nvim',
+        events = { 'BufReadPost', 'BufNewFile' },
         opts = {
             signcolumn = true,
             current_line_blame = true,
             attach_to_untracked = false,
+            current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
             watch_gitdir = {
                 follow_files = true,
             },
             signs = {
-                add = { text = " " },
-                change = { text = "󱗜 " },
-                delete = { text = "󰍵 " },
-                topdelete = { text = "󱥨 " },
-                changedelete = { text = "󰾟 " },
-                untracked = { text = "󰰧 " },
+                add = { text = ' ' },
+                change = { text = '󱗜 ' },
+                delete = { text = '󰍵 ' },
+                topdelete = { text = '󱥨 ' },
+                changedelete = { text = '󰾟 ' },
+                untracked = { text = '󰰧 ' },
             },
         },
     },
 
     -- Todo Comments
     {
-        "folke/todo-comments.nvim",
-        event = "BufRead",
+        'folke/todo-comments.nvim',
+        event = 'BufRead',
         opts = {},
     },
 
     -- Working with file permissions
     {
-        "lambdalisue/vim-suda",
-        cmd = { "SudaRead", "SudaWrite" },
+        'lambdalisue/vim-suda',
+        cmd = { 'SudaRead', 'SudaWrite' },
     },
 
     -- Undo tree
     {
-        "mbbill/undotree",
-        cmd = "UndotreeToggle",
-        keys = { { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Undo Tree" } },
+        'mbbill/undotree',
+        cmd = 'UndotreeToggle',
+        keys = { { '<leader>u', '<cmd>UndotreeToggle<cr>', desc = 'Undo Tree' } },
     },
 
     -- visual multi
     {
-        "mg979/vim-visual-multi",
-        event = "VeryLazy",
+        'mg979/vim-visual-multi',
+        event = 'VeryLazy',
         init = function()
             vim.g.VM_maps = {
-                ["Find Under"] = "<M-d>",
-                ["Find Subword Under"] = "<M-d>",
-                ["Select All"] = "<C-M-d>",
-                ["Add Cursor Down"] = "<C-M-j>",
-                ["Add Cursor Up"] = "<C-M-k>",
+                ['Find Under'] = '<M-d>',
+                ['Find Subword Under'] = '<M-d>',
+                ['Select All'] = '<C-M-d>',
+                ['Add Cursor Down'] = '<C-M-j>',
+                ['Add Cursor Up'] = '<C-M-k>',
             }
         end,
     },
 
     -- dimming the highlights of unused functions, variables, parameters, and more
     {
-        "zbirenbaum/neodim",
+        'zbirenbaum/neodim',
         enabled = false,
-        event = "LspAttach",
+        event = 'LspAttach',
         config = function()
-            require("neodim").setup({
+            require('neodim').setup({
                 alpha = 0.5,
                 blend_color = nil,
                 hide = {
@@ -78,9 +79,9 @@ return {
                     signs = true,
                 },
                 regex = {
-                    "[uU]nused",
-                    "[nN]ever [rR]ead",
-                    "[nN]ot [rR]ead",
+                    '[uU]nused',
+                    '[nN]ever [rR]ead',
+                    '[nN]ot [rR]ead',
                 },
                 priority = 128,
                 disable = {},
@@ -90,79 +91,79 @@ return {
 
     -- file operations
     {
-        "chrisgrieser/nvim-genghis",
+        'chrisgrieser/nvim-genghis',
         keys = {
             {
-                "<leader>fn",
+                '<leader>fn',
                 function()
-                    require("genghis").createNewFile()
+                    require('genghis').createNewFile()
                 end,
-                desc = "New File",
+                desc = 'New File',
             },
             {
-                "<leader>fd",
+                '<leader>fd',
                 function()
-                    require("genghis").duplicateFile()
+                    require('genghis').duplicateFile()
                 end,
-                desc = "Duplicate File",
+                desc = 'Duplicate File',
             },
             {
-                "<leader>fr",
+                '<leader>fr',
                 function()
-                    require("genghis").renameFile()
+                    require('genghis').renameFile()
                 end,
-                desc = "Rename File",
+                desc = 'Rename File',
             },
             {
-                "<leader>fx",
+                '<leader>fx',
                 function()
-                    require("genghis").chmodx()
+                    require('genghis').chmodx()
                 end,
-                desc = "Make Executable",
+                desc = 'Make Executable',
             },
         },
     },
 
     -- Better folding
     {
-        "kevinhwang91/nvim-ufo",
-        dependencies = "kevinhwang91/promise-async",
-        event = "BufReadPost",
+        'kevinhwang91/nvim-ufo',
+        dependencies = 'kevinhwang91/promise-async',
+        event = 'BufReadPost',
         keys = {
             {
-                "zR",
+                'zR',
                 function()
-                    require("ufo").openAllFolds()
+                    require('ufo').openAllFolds()
                 end,
             },
             {
-                "zM",
+                'zM',
                 function()
-                    require("ufo").closeAllFolds()
+                    require('ufo').closeAllFolds()
                 end,
             },
             {
-                "zr",
+                'zr',
                 function()
-                    require("ufo").openFoldsExceptKinds()
+                    require('ufo').openFoldsExceptKinds()
                 end,
             },
             {
-                "zm",
+                'zm',
                 function()
-                    require("ufo").closeFoldsWith()
+                    require('ufo').closeFoldsWith()
                 end,
             },
         },
         config = function()
-            vim.o.foldcolumn = "1"
+            vim.o.foldcolumn = '1'
             vim.o.foldlevel = 99
             vim.o.foldlevelstart = 99
             vim.o.foldenable = true
 
-            require("ufo").setup({
+            require('ufo').setup({
                 provider_selector = function()
-                    return { "treesitter", "indent" }
+                    return { 'treesitter', 'indent' }
                 end,
             })
         end,
@@ -170,7 +171,7 @@ return {
 
     -- rendering for html and markdown
     {
-        "OXY2DEV/markview.nvim",
+        'OXY2DEV/markview.nvim',
         lazy = false,
         -- to load before treesitter
         priority = 45,
@@ -180,18 +181,18 @@ return {
             },
         },
         keys = {
-            { "<leader>cv", "<Cmd>Markview toggle<CR>", desc = "Toggle markdown preview", mode = "n" },
+            { '<leader>cv', '<Cmd>Markview toggle<CR>', desc = 'Toggle markdown preview', mode = 'n' },
         },
     },
 
     -- markdown preview
     {
-        "iamcco/markdown-preview.nvim",
-        build = "cd app && yarn install",
+        'iamcco/markdown-preview.nvim',
+        build = 'cd app && yarn install',
         init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
+            vim.g.mkdp_filetypes = { 'markdown' }
         end,
-        ft = "markdown",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview" },
+        ft = 'markdown',
+        cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview' },
     },
 }
